fdbd3ca4ead182e2b3d9acd9876dfa38d2d8b8a2
leegh1025 <leegh1025@naver.com>
2024-09-27 18:19:53 +0900

    [스웨거에 Authorization 추가해서 토큰 인증후 확인]

--- 4th/src/main/java/com/pironeer/_th/global/config/OpenApiConfig.java
+++ 4th/src/main/java/com/pironeer/_th/global/config/OpenApiConfig.java
@@ -2,8 +2,11 @@ package com.pironeer._th.global.config;
 
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
@@ -15,7 +18,15 @@ import org.springframework.context.annotation.Configuration;
                         name = "이건희",
                         email = "leegh1925@gmail.com"
                 )
-        )
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+
+@SecurityScheme(
+        name = "bearerAuth",      // 보안 스키마 이름
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",        // Bearer 토큰 방식 사용
+        bearerFormat = "JWT"      // JWT 형식
 )
 
 @Configuration
--- 4th/src/main/java/com/pironeer/_th/global/config/WebConfig.java
+++ 4th/src/main/java/com/pironeer/_th/global/config/WebConfig.java
@@ -16,6 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(jwtInterceptor).addPathPatterns("/api/board/**");
+        registry.addInterceptor(jwtInterceptor)
+                .addPathPatterns("/api/board/**");
     }
 }
